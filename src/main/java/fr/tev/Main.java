9d3339e5bdb8bfbf3232b65029eeef4c2ecc4ca4
ezacuak <antonin.plard@protonmail.com>
2024-12-13 17:16:20 +0100

    Premiere fenetre

--- src/main/java/fr/tev/Main.java
+++ src/main/java/fr/tev/Main.java
@@ -1,7 +1,21 @@
 package fr.tev;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!");
+        // Create a window (JFrame)
+        JFrame frame = new JFrame("My First Swing App");
+
+        // Set size and close operation
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Add a label
+        JLabel label = new JLabel("Hello, Swing!", SwingConstants.CENTER);
+        frame.add(label);
+
+        // Make the frame visible
+        frame.setVisible(true);
     }
 }
