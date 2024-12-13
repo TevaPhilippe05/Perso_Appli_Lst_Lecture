887e0871a2e7ef9bf39215ee0bab6355d724bcd0
ezacuak <antonin.plard@protonmail.com>
2024-12-13 17:26:15 +0100

    Ajoute de la methode start() dans App

--- src/main/java/fr/tev/App.java
+++ src/main/java/fr/tev/App.java
@@ -1,4 +1,19 @@
 package fr.tev;
 
+import javax.swing.*;
+
 public class App {
+    private String name;
+    private JFrame window;
+
+    public App(String name) {
+        this.name = name;
+        this.window = new JFrame(this.name);
+    }
+
+    public void start() {
+        this.window.setSize(800, 800);
+        this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.window.setVisible(true);
+    }
 }
--- src/main/java/fr/tev/Main.java
+++ src/main/java/fr/tev/Main.java
@@ -5,17 +5,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         // Create a window (JFrame)
-        JFrame frame = new JFrame("My First Swing App");
+        App app = new App("Lecture");
 
-        // Set size and close operation
-        frame.setSize(400, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Add a label
-        JLabel label = new JLabel("Hello, Swing!", SwingConstants.CENTER);
-        frame.add(label);
-
-        // Make the frame visible
-        frame.setVisible(true);
+        app.start();
     }
 }
